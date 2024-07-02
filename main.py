3026718c24d7b056a9b114b6b2178449f2b31857
hasibthayattuchira <hthayattuchira@gmail.com>
2024-07-02 09:09:50 -0400

    Create main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ def main():
     while True:
         start = input('Enter the starting letter: ')
         end = input('Enter the starting letter: ')
+        
+        if not start.isalpha() or not end.isalpha():
+            print('Must be an alphabet ')
+            continue
+        
+        if ord(start) > ord(end):
+            print('Start letter should be < end letter')
+            continue
+        
+        for i in range(ord(start), ord(end) + 1):
+            result.append(chr(i))
+        
 
     """
     ########################################
