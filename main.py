377cafbc81998bece41b4a70c0d08ed8b5e21799
hasibthayattuchira <hthayattuchira@gmail.com>
2024-07-02 06:24:59 -0700

    Create main.py and consecutive.drawio

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
         
         for i in range(ord(start), ord(end) + 1):
             result.append(chr(i))
+        print(*result)
+        break
         
 
     """
@@ -22,7 +24,7 @@ def main():
     ########################################
     """
 
-    print(*result)
+
 
     ########################################
     # Do not delete the return statement
